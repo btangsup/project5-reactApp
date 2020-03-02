fecf50c06a125a26ab4511cebd8d71052642d00d
Brandon-Tang <btangsup@gmail.com>
2020-03-02 17:01:09 -0500

    Updates

---
+++ precache-manifest.768e2789ea6242f76709df1ddedc4e4f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3db8d8ee4e2373531d4161ddddcfd931",
+    "url": "/project5-reactApp/index.html"
+  },
+  {
+    "revision": "576b20e1ae76cdfa6692",
+    "url": "/project5-reactApp/static/css/main.b9179683.chunk.css"
+  },
+  {
+    "revision": "58abfc9e41496b2545aa",
+    "url": "/project5-reactApp/static/js/2.224d5401.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/project5-reactApp/static/js/2.224d5401.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "576b20e1ae76cdfa6692",
+    "url": "/project5-reactApp/static/js/main.c738b826.chunk.js"
+  },
+  {
+    "revision": "9c78da097900af994fcd",
+    "url": "/project5-reactApp/static/js/runtime-main.6b007c8c.js"
+  },
+  {
+    "revision": "f1f414da5c386f1b6517983d9ec7ce4b",
+    "url": "/project5-reactApp/static/media/blackWhite-logo.f1f414da.png"
+  },
+  {
+    "revision": "98424f33f9b5a846bd14cecc7d659d8f",
+    "url": "/project5-reactApp/static/media/header.98424f33.jpg"
+  }
+]);
